f8ec31069d7082c87ad9e94ad40c257b84bfc8f3
rimit-rim <sunsor0105@naver.com>
2024-11-18 17:48:43 +0900

    First BE branch

--- tantan/main/models.py
+++ tantan/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)  # 사용자 이름 (유니크)
+    email = models.EmailField(unique=True)  # 이메일 (유니크)
+    password = models.CharField(max_length=128)  # 비밀번호 (해시 형태로 저장)
+    created_at = models.DateTimeField(auto_now_add=True)  # 계정 생성 날짜
+
+    def __str__(self):
+        return self.username
